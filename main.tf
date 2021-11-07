3c83da4e31170874dffcb604fd032a4652252f7f
migmichels <mmichels664@gmail.com>
2021-11-07 20:53:22 +0000

    step 5

--- main.tf
+++ main.tf
@@ -7,19 +7,51 @@ provider "aws" {
 # 1. Create vpc
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
 }
 
 # 2. Create Internet Gateway
-
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
 
 # 3. Create Custom Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
 
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
 
 # 4. Create a Subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
+  tags = {
+    Name = "prod-subnet"
+  }
+}
 
 # 5. Associate subnet with Route Table
-
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
 
 # 6. Create Security Group to allow port 22, 80, 443
 
