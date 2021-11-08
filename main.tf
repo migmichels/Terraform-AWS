561675060b1fd8cb59c0e1037fbe9fd9d548f248
migmichels <mmichels664@gmail.com>
2021-11-08 19:56:48 +0000

    added server private ip and server id on output

--- main.tf
+++ main.tf
@@ -146,3 +146,12 @@ resource "aws_instance" "web-server-instance" {
     Name = "web-server"
   }
 }
+
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
+}
